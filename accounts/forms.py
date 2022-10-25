82e49953340e3ca10da22ed0613f2189127c4329
Lauren Lowe <lauren.lowe84@gmail.com>
2022-10-25 14:56:22 -0700

    Feature 10 complete

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,3 +5,12 @@ class LogInForm(forms.Form):
     password=forms.CharField(
         max_length=150,
         widget=forms.PasswordInput,)
+
+class SignUpForm(forms.Form):
+    username=forms.CharField(max_length=150)
+    password=forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput,)
+    password_confirmation=forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput,)
