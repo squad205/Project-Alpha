ee9190ee413adfc0951e710288e17ff1b9a403a4
Lauren Lowe <lauren.lowe84@gmail.com>
2022-10-26 19:42:49 -0700

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from projects.views import project_list, show_project
+from projects.views import project_list, show_project, create_project
 
 urlpatterns = [
     path("", project_list, name="list_projects"),
     path("<int:id>/", show_project, name="show_project"),
+    path("create/", create_project, name="create_project"),
 ]
